a9e9c2ceb0db9a0faef90c8c8c8f15401c48977b
0gust1 <augustin.chassine@gmail.com>
2015-05-06 08:31:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Quelques scripts pour commencer à s'amuser avec des sources d'images.
+
+Actuellement, juste quelques fonctions pour 
+
+* interroger l'API flickr et traiter les réponses
+* télécharger efficacement une liste d'images
+* télécharger une liste d'images depuis un RSS tumblr
+
+Pour l'utiliser (la partie flickr), vous aurez besoin d'une clef pour l'API (et donc d'avoir un compte Flickr) : créez un fichier `flickrKey.json` à la raçine de votre projet, suivant le modèle suivant :
+
+```
+{
+      "api_key": "azertyuiop1234567890",
+      "secret": "azertyuiop1234567890"
+}
+```
---
+++ index.js
@@ -0,0 +1,239 @@
+/**
+ * convert -normalize -modulate 100,0 leo.jpg test3.jpg
+ * http://seenthis.net/messages/341357
+ * https://github.com/KeyboardFire/mkcast
+ * http://sebsauvage.net/python/webgobbler/index.html
+ *
+ * http://trackingjs.com/docs.html
+ * http://inspirit.github.io/jsfeat/
+ *
+ * https://github.com/auduno/clmtrackr
+ * http://alereimondo.no-ip.org/OpenCV/34
+ * http://docs.opencv.org/doc/tutorials/features2d/feature_detection/feature_detection.html#feature-detection
+ */
+
+
+var Flickr = require("flickrapi");
+var flickrOptions = require('./flickrKey.json');
+
+var Q = require("q");
+var _ = require("lodash");
+var path = require("path");
+var request = require("request");
+var parseRSS = require("parse-rss");
+var fUrl = require('flickr-urls');
+
+var DL_DIR = "./dl";
+
+// Flickr.authenticate(flickrOptions, function(error, flickr) {
+//   flickr.photos.search({
+//   user_id: flickr.options.user_id,
+//   page: 1,
+//   per_page: 500
+// }, function(err, result) {
+//   console.log(result);
+// });
+// });
+
+function parseTumblrFeed(url) {
+  var defer = Q.defer();
+  parseRSS(url, function(err, rss) {
+    if (err) {
+      throw err;
+    }
+    defer.resolve(rss);
+  });
+  return defer.promise;
+}
+
+/*parseTumblrFeed("http://archillect.com/rss").then(
+		function(rss){
+			var imgList = rss.map(function(e){return e.summary.match(/src="([^"]*)"/i)[1]});
+			console.log(imgList);
+			downloadPictures(imgList);
+			//console.log()
+		}
+	);*/
+
+Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+
+  console.log("flickr options : ", flickr.options);
+
+  var searchOptions = {
+    is_commons: true,
+    //tags:'sorcery',
+    text: "dharma",
+    sort: "interestingness-desc",
+    safe_search: 3,
+    page: 1,
+    per_page: 5,
+    extras: "description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o"
+  };
+
+  var gettingsearch = getFlickrSearchFullResults(flickr, searchOptions);
+  gettingsearch.then(function(v) {
+    var cleaned = _.compact(v);
+    console.log("final result : \n", cleaned);
+    downloadPictures(cleaned.map(function(e) {
+      return e.url_l
+    }));
+    //console.log(JSON.stringify(cleaned, null, 2));
+  });
+});
+
+/**
+ * Perform a flickr search with provided options
+ * Filter out bad URLs
+ *
+ * Returns an array of verified, detailed photo objects
+ *
+ * @param  {[type]} flickr        [description]
+ * @param  {[type]} searchOptions [description]
+ * @return {[type]}               [description]
+ */
+function getFlickrSearchResults(flickr, searchOptions) {
+  var defer = Q.defer();
+  flickr.photos.search(searchOptions,
+    function(err, result) {
+      if (err) {
+        throw err;
+      }
+      var photoList = result.photos.photo;
+      //return the result with tested url (as flickr static servers are not able to send correct http codes)
+      console.log(photoList);
+      if (photoList.length > 0) {
+        photoList = getFlickrUrl(photoList);
+        defer.resolve(removeInvalidPhotos(photoList, 'targetUrl'));
+      } else {
+        defer.resolve(photoList);
+      }
+    }
+  );
+  return defer.promise;
+}
+;
+
+/**
+ * Perform a complete flickr search with provided options
+ * For each photo, get the detailed info
+ * Filter out bad URLs
+ *
+ * Returns an array of verified, detailed photo objects
+ *
+ * @param  {[type]} flickr        [description]
+ * @param  {[type]} searchOptions [description]
+ * @return {[type]}               [description]
+ */
+function getFlickrSearchFullResults(flickr, searchOptions) {
+  var defer = Q.defer();
+  flickr.photos.search(searchOptions,
+    function(err, result) {
+      if (err) {
+        throw err;
+      }
+      var finRes = null;
+
+      var photoList = result.photos.photo;
+
+      var gettingInfos = getPhotosInfosFromSearch(photoList, flickr);
+
+      gettingInfos.then(function(val) {
+        finRes = processPhotosInfos(val.map(function(e) {
+          return e.photo;
+        }), photoList);
+        finRes = getFlickrUrl(finRes);
+      }).done(function() {
+        defer.resolve(removeInvalidPhotos(finRes, 'targetUrl'));
+      });
+    }
+  );
+  return defer.promise;
+}
+
+function getFlickrUrl(list) {
+  return list.map(function(el) {
+    el.targetUrl = getFUrl(el);
+    return el;
+  });
+
+  function getFUrl(e) {
+    var params = {
+      server: e.server,
+      secret: e.secret,
+      id: e.id,
+      farm: e.farm,
+      size: fUrl.IMG_SIZES.LARGE_1024
+    };
+    return fUrl.getFarmUrl(params);
+  }
+}
+
+function removeInvalidPhotos(photoCollection, urlField) {
+  var https = require('https');
+
+  var nextColl = photoCollection.map(function(e) {
+    var defer = Q.defer();
+    var urlObj = require('url').parse(e[urlField]);
+    var requestOpts = {
+      host: urlObj.host,
+      path: urlObj.path,
+      method: "HEAD"
+    };
+
+    var req = https.request(requestOpts, function(res) {
+      if (res.headers.location) {
+        //console.log("unavaiable : ",urlObj.path);
+        res.destroy();
+        defer.resolve(null);
+      } else {
+        defer.resolve(e);
+      }
+    });
+    req.end();
+
+    return defer.promise;
+  });
+
+  return Q.all(nextColl);
+}
+
+/**
+ * Get a full collection of photo infos from a flickr search result
+ * @param  {[type]} photoList [description]
+ * @param  {[type]} flickr    [description]
+ * @return {Promise}           [description]
+ */
+function getPhotosInfosFromSearch(photoList, flickr) {
+
+  var gettingAllInfos = photoList.map(function(elem) {
+
+    return Q.nfcall(flickr.photos.getInfo, {
+      'photo_id': elem.id
+    });
+  });
+
+  return Q.all(gettingAllInfos);
+}
+
+function processPhotosInfos(photoInfo, photoList) {
+  _.merge(photoList, photoInfo);
+  return photoList;
+}
+
+function downloadPictures(photoList) {
+  var fs = require('fs');
+
+  photoList.forEach(function(e) {
+    var fileName = getFileName(e);
+    request(e).pipe(fs.createWriteStream(path.resolve(DL_DIR, fileName)));
+  });
+
+  function getFileName(path) {
+    return path.match(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)(:([^\/]*))?((\/[\w/-]+)*\/)([\w\-\.]+[^#?\s]+)(\?([^#]*))?(#(.*))?$/i)[8];
+  }
+}
+
+
+//Get all th contributors to commons
+//For each contributor, get the setList
+//For each setlist (or the most faved/commented), get the most faved commented view photos
