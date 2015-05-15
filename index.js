5755e74530441dc52d5f582c93e1a98f596096f3
0gust1 <augustin.chassine@gmail.com>
2015-05-15 21:35:36 +0200

    refacto

--- index.js
+++ index.js
@@ -13,9 +13,6 @@
  */
 
 
-var Flickr = require("flickrapi");
-var flickrOptions = require('./flickrKey.json');
-
 var Q = require("q");
 var _ = require("lodash");
 var path = require("path");
@@ -35,6 +32,11 @@ var DL_DIR = "./dl";
 // });
 // });
 
+/**
+ * [parseTumblrFeed description]
+ * @param  {String} url [description]
+ * @return {Promise}    Promise for the JS object representing the feed
+ */
 function parseTumblrFeed(url) {
   var defer = Q.defer();
   parseRSS(url, function(err, rss) {
@@ -46,194 +48,17 @@ function parseTumblrFeed(url) {
   return defer.promise;
 }
 
-/*parseTumblrFeed("http://archillect.com/rss").then(
-		function(rss){
-			var imgList = rss.map(function(e){return e.summary.match(/src="([^"]*)"/i)[1]});
-			console.log(imgList);
-			downloadPictures(imgList);
-			//console.log()
-		}
-	);*/
+var flickrSearch = require("./lib/flickr.js");
 
-Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-
-  console.log("flickr options : ", flickr.options);
-
-  var searchOptions = {
-    is_commons: true,
-    //tags:'sorcery',
-    text: "dharma",
-    sort: "interestingness-desc",
-    safe_search: 3,
-    page: 1,
-    per_page: 5,
-    extras: "description, license, date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_q, url_m, url_n, url_z, url_c, url_l, url_o"
-  };
-
-  var gettingsearch = getFlickrSearchFullResults(flickr, searchOptions);
-  gettingsearch.then(function(v) {
-    var cleaned = _.compact(v);
-    console.log("final result : \n", cleaned);
-    downloadPictures(cleaned.map(function(e) {
-      return e.url_l
-    }));
-    //console.log(JSON.stringify(cleaned, null, 2));
-  });
-});
-
-/**
- * Perform a flickr search with provided options
- * Filter out bad URLs
- *
- * Returns an array of verified, detailed photo objects
- *
- * @param  {[type]} flickr        [description]
- * @param  {[type]} searchOptions [description]
- * @return {[type]}               [description]
- */
-function getFlickrSearchResults(flickr, searchOptions) {
-  var defer = Q.defer();
-  flickr.photos.search(searchOptions,
-    function(err, result) {
-      if (err) {
-        throw err;
-      }
-      var photoList = result.photos.photo;
-      //return the result with tested url (as flickr static servers are not able to send correct http codes)
-      console.log(photoList);
-      if (photoList.length > 0) {
-        photoList = getFlickrUrl(photoList);
-        defer.resolve(removeInvalidPhotos(photoList, 'targetUrl'));
-      } else {
-        defer.resolve(photoList);
-      }
-    }
-  );
-  return defer.promise;
-}
-;
-
-/**
- * Perform a complete flickr search with provided options
- * For each photo, get the detailed info
- * Filter out bad URLs
- *
- * Returns an array of verified, detailed photo objects
- *
- * @param  {[type]} flickr        [description]
- * @param  {[type]} searchOptions [description]
- * @return {[type]}               [description]
- */
-function getFlickrSearchFullResults(flickr, searchOptions) {
-  var defer = Q.defer();
-  flickr.photos.search(searchOptions,
-    function(err, result) {
-      if (err) {
-        throw err;
-      }
-      var finRes = null;
-
-      var photoList = result.photos.photo;
-
-      var gettingInfos = getPhotosInfosFromSearch(photoList, flickr);
-
-      gettingInfos.then(function(val) {
-        finRes = processPhotosInfos(val.map(function(e) {
-          return e.photo;
-        }), photoList);
-        finRes = getFlickrUrl(finRes);
-      }).done(function() {
-        defer.resolve(removeInvalidPhotos(finRes, 'targetUrl'));
-      });
-    }
-  );
-  return defer.promise;
-}
-
-function getFlickrUrl(list) {
-  return list.map(function(el) {
-    el.targetUrl = getFUrl(el);
-    return el;
-  });
-
-  function getFUrl(e) {
-    var params = {
-      server: e.server,
-      secret: e.secret,
-      id: e.id,
-      farm: e.farm,
-      size: fUrl.IMG_SIZES.LARGE_1024
-    };
-    return fUrl.getFarmUrl(params);
-  }
-}
-
-function removeInvalidPhotos(photoCollection, urlField) {
-  var https = require('https');
-
-  var nextColl = photoCollection.map(function(e) {
-    var defer = Q.defer();
-    var urlObj = require('url').parse(e[urlField]);
-    var requestOpts = {
-      host: urlObj.host,
-      path: urlObj.path,
-      method: "HEAD"
-    };
-
-    var req = https.request(requestOpts, function(res) {
-      if (res.headers.location) {
-        //console.log("unavaiable : ",urlObj.path);
-        res.destroy();
-        defer.resolve(null);
-      } else {
-        defer.resolve(e);
-      }
-    });
-    req.end();
-
-    return defer.promise;
-  });
-
-  return Q.all(nextColl);
-}
-
-/**
- * Get a full collection of photo infos from a flickr search result
- * @param  {[type]} photoList [description]
- * @param  {[type]} flickr    [description]
- * @return {Promise}           [description]
- */
-function getPhotosInfosFromSearch(photoList, flickr) {
-
-  var gettingAllInfos = photoList.map(function(elem) {
-
-    return Q.nfcall(flickr.photos.getInfo, {
-      'photo_id': elem.id
-    });
-  });
-
-  return Q.all(gettingAllInfos);
-}
-
-function processPhotosInfos(photoInfo, photoList) {
-  _.merge(photoList, photoInfo);
-  return photoList;
-}
-
-function downloadPictures(photoList) {
-  var fs = require('fs');
-
-  photoList.forEach(function(e) {
-    var fileName = getFileName(e);
-    request(e).pipe(fs.createWriteStream(path.resolve(DL_DIR, fileName)));
-  });
-
-  function getFileName(path) {
-    return path.match(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)(:([^\/]*))?((\/[\w/-]+)*\/)([\w\-\.]+[^#?\s]+)(\?([^#]*))?(#(.*))?$/i)[8];
-  }
-}
+var searchOptions = {
+  text: "pilot",
+  sort: "interestingness-desc",
+  safe_search: 3,
+  page: 1,
+  per_page: 5
+};
 
 
-//Get all th contributors to commons
-//For each contributor, get the setList
-//For each setlist (or the most faved/commented), get the most faved commented view photos
+console.log(flickrSearch.searchCommons(searchOptions));
+
+
