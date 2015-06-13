40bf6cc1c1881d8fa3172a1e915d28b1b8ee9306
0gust1 <augustin.chassine@gmail.com>
2015-06-13 21:38:32 +0200

    wip search

--- index.js
+++ index.js
@@ -14,17 +14,16 @@
 
 
 var Q = require("q");
-var _ = require("lodash");
-var path = require("path");
-var request = require("request");
-var parseRSS = require("parse-rss");
-var url = require("url");
-var path = require("path");
-var fUrl = require('flickr-urls');
-var https = require("https");
-  	var url = require("url");
-
-var DL_DIR = "./dl";
+_ = require("lodash"),
+path = require("path"),
+request = require("request"),
+parseRSS = require("parse-rss"),
+url = require("url"),
+path = require("path"),
+fUrl = require('flickr-urls'),
+https = require("https"),
+url = require("url"),
+DL_DIR = "./dl";
 
 // Flickr.authenticate(flickrOptions, function(error, flickr) {
 //   flickr.photos.search({
@@ -55,11 +54,12 @@ function parseTumblrFeed(url) {
 var flickrSearch = require("./lib/flickr.js");
 
 var searchOptions = {
-  text: "farmer",
+  user_id:"me",
+  //text: "sculpture",
   sort: "interestingness-desc",
   safe_search: 3,
   page: 1,
-  per_page: 20
+  per_page: 10
 };
 
 // var toto = flickrSearch.searchCommons(searchOptions);
@@ -76,79 +76,64 @@ var Image = Canvas.Image;
 var fs = require('fs');
 var size = 1024;
 
-var request = require("request");
+searchFlickrAndExtractFaces(searchOptions);
 
-var search = flickrSearch.searchCommons(searchOptions);
+function searchFlickrAndExtractFaces(searchOptions) {
 
-search.then(function(res) {
-  //console.log(res);
+  var search = flickrSearch.searchFlickr(searchOptions);
 
-  var infos = res.map(function(element, index){
-  	var el = {};
-  	el.id = element.id;
-  	el.targetUrl = element.targetUrl;
-  	el.title = element.title;
-  	return el;
+  search.then(function(res) {
+    //console.log(res);
+
+    var infos = res.map(function(element, index) {
+      console.log(element);
+      var el = {};
+      el.id = element.id;
+      el.targetUrl = element.targetUrl;
+      el.title = element.title;
+      return el;
+    });
+
+    infos.forEach(function(element, index) {
+      console.log("ix : ", element);
+
+      var options = url.parse(element.targetUrl);
+
+      https.get(options, function(response) {
+        var chunks = [];
+        response.on('data', function(chunk) {
+          chunks.push(chunk);
+        }).on('end', function() {
+          //console.log(response);
+          var buffer = Buffer.concat(chunks);
+          console.log("file : " + buffer.length);
+          console.log("link : ", element.targetUrl);
+          processImage(null, buffer, element);
+        });
+      });
+
+    }); //end foreach
+
   });
+}
 
-  infos.forEach(function(element, index){
+function getImageFile(path, callback) {
 
+}
 
- //  	request.get(element.targetUrl,function(error,response,body){
-	// 	console.log(response.statusCode);
-	//     console.log(response.headers['content-type']);
-	//     if (!error && response.statusCode == 200) {
- //        processImage(null,body);
- //        // Continue with your processing here.
- //    	}
-	//     //console.log("response : ",response);
+function getUrl(imageUrl, fprocess, infos) {
+  request.get(imageUrl).on('response', function(response) {
+    console.log(response.statusCode);
+    console.log(response.headers['content-type']);
+    console.log("response : ", response);
 
-	//     //
-	// });
-  	console.log("ix : ",element);
-	// request.get(element.targetUrl,function(err,response,body){
-	// 	//console.log(response);
-	// 	//console.log(body);
-
-	// 	response.on("end",function(error,response,body){console.log("hzy");});
-	//     //console.log(response.headers['content-type']);
-	//     if (!err){//} && response.statusCode == 200) {
- //        processImage(null,body,element);
- //        // Continue with your processing here.
- //    	}
- //  });
-
-  	var options = url.parse(element.targetUrl);
-	https.get(options, function (response) {
-	  var chunks = [];
-	  response.on('data', function (chunk) {
-	    chunks.push(chunk);
-	  }).on('end', function() {
-	  	//console.log(response);
-	    var buffer = Buffer.concat(chunks);
-	    console.log("file : "+buffer.length);
-	    console.log("link : ",element.targetUrl);
-	    processImage(null,buffer,element);
-	  });
-	});
-
-});//end foreach
-
-});
-
-function getUrl(imageUrl, fprocess,infos){
-	request.get(imageUrl).on('response',function(response){
-		console.log(response.statusCode);
-	    console.log(response.headers['content-type']);
-	    console.log("response : ",response);
-
-	    fprocess.call(response);
-	});
+    fprocess.call(response);
+  });
 }
 
 //fs.readFile(__dirname + '/dl/group4.jpg', processImage);
 
-function processImage(err, buff, infos){
+function processImage(err, buff, infos) {
   //console.log("buf : ",buff);
   var canvas = new Canvas();
   var ctx = canvas.getContext('2d');
@@ -165,22 +150,20 @@ function processImage(err, buff, infos){
 
   detector = new objdetect.detector(canvas.width, canvas.height, 1.25, objdetect.frontalface_alt);
 
-  var rects = detectFaces(canvas).filter(function(e){
-  	return e[4]>10 && e[2]/img.width > 0.006;
-  	//return true;
+  var rects = detectFaces(canvas).filter(function(e) {
+    return e[4] > 10 && e[2] / img.width > 0.006;
   });
 
-  rects.map(function(e,i){
-  var canFace = new Canvas();
+  //crop and save faces
+  rects.map(function(e, i) {
+    var canFace = new Canvas();
 
-  canFace.width =e[2];
-  canFace.height =e[3]*1.618;
-  canFace.getContext('2d').drawImage(canvas, e[0], e[1]-(e[3]*0.309), e[2], e[3]*1.618, 0, 0,e[2],e[3]*1.618);
+    canFace.width = e[2];
+    canFace.height = e[3] * 1.618;
+    canFace.getContext('2d').drawImage(canvas, e[0], e[1] - (e[3] * 0.309), e[2], e[3] * 1.618, 0, 0, e[2], e[3] * 1.618);
 
-  saveCanvas(canFace,"dl/",infos.id+"_"+i);
+    saveCanvas(canFace, "dl/", infos.id + "_" + i);
   });
-
-
 
   /**
    * [detectFaces description]
@@ -189,7 +172,7 @@ function processImage(err, buff, infos){
    */
   function detectFaces(canvas) {
     // Detect faces in the image:
-    var rects = detector.detect(canvas,1);
+    var rects = detector.detect(canvas, 1);
     //console.log("detector : ", detector.tilted);
     //console.log("dtected rectangles : ", rects);
 
@@ -217,14 +200,14 @@ function processImage(err, buff, infos){
     //   ctx.stroke();
     // }
     return rects;
-    //draw mean rectangles
-    //detector.groupRectangles();
+  //draw mean rectangles
+  //detector.groupRectangles();
   }
 
 
   function saveCanvas(canvas, path, name) {
     var fst = require('fs'),
-      out = fst.createWriteStream(__dirname +"/"+ path + name +'.jpg'),
+      out = fst.createWriteStream(__dirname + "/" + path + name + '.jpg'),
       stream = canvas.jpegStream();
 
     stream.on('data', function(chunk) {
@@ -232,14 +215,9 @@ function processImage(err, buff, infos){
     });
 
     stream.on('end', function() {
-      console.log('saved '+__dirname +"/"+ path + name +'.jpg');
+      console.log('saved ' + __dirname + "/" + path + name + '.jpg');
     });
 
   }
-
-
-
-
-
 
 }
